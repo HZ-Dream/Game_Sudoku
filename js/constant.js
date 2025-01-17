3599a964b7a7430a67ddc46bfc65377a0f4d539b
Ngoc Hoai <ngochoai5813@gmail.com>
2025-01-17 22:54:42 +0700

    Complete Code

--- js/constant.js
+++ js/constant.js
@@ -2,7 +2,7 @@ const CONSTANT = {
     UNASSIGNED: 0,
     GRID_SIZE: 9,
     BOX_SIZE: 3,
-    Number: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    NUMBERS: [1,2,3,4,5,6,7,8,9],
     LEVEL_NAME: [
         'Easy',
         'Medium',
